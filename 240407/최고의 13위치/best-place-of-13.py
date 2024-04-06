f797f2d486fbba2fd55221e407343dd0445a75f4
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-07 01:01:31 +0900

    Title: 최고의 13위치, Time: 151ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240407/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [list(map(int, input().split())) for i in range(n)]
+result = 0
+
+if n == 3:
+    for line in arr:
+        result = max(result, sum(line))
+else:
+    for i in range(n):
+        for j in range(n - 3):
+            result = max(result, arr[i][j] + arr[i][j+1] + arr[i][j+2])
+
+print(result)
