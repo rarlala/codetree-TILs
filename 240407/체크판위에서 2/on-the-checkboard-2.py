efa2ca0726fdad8224a6006995d23365154689f7
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-07 22:20:59 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240407/체크판위에서 2/on-the-checkboard-2.py
+++ 240407/체크판위에서 2/on-the-checkboard-2.py
@@ -1,16 +1,12 @@
 r, c = map(int, input().split())
-arr = [list(input().split()) for _ in range(r)]
+arr = [input().split() for _ in range(r)]
 result = 0
 
-stack = [[0, 0, 0]]
-while stack:
-    x, y, check = stack.pop()
-    for i in range(x + 1, r):
-        for j in range(y + 1, c):
-            if i == r - 1 and j == c - 1:
-                result += 1
-            if arr[i][j] != arr[x][y] and check < 2:
-                stack.append([i, j, check + 1])
-                break
-            
+for i in range(1, r):
+    for j in range(1, c):
+        for k in range(i + 1, r - 1):
+            for l in range(j + 1, c - 1):
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[k][l] and arr[k][l] != arr[r - 1][c - 1]:
+                    count += 1
+
 print(result)
