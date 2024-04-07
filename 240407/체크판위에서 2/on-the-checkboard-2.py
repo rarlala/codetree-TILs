6e8279011cd731b7e9a32865777fe73d2d62c84c
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-07 22:21:19 +0900

    Title: 체크판위에서 2, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240407/체크판위에서 2/on-the-checkboard-2.py
+++ 240407/체크판위에서 2/on-the-checkboard-2.py
@@ -7,6 +7,6 @@ for i in range(1, r):
         for k in range(i + 1, r - 1):
             for l in range(j + 1, c - 1):
                 if arr[0][0] != arr[i][j] and arr[i][j] != arr[k][l] and arr[k][l] != arr[r - 1][c - 1]:
-                    count += 1
+                    result += 1
 
 print(result)
