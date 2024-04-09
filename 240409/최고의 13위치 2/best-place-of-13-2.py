617129b0da483c2742cf8a2e104bcb0dd555a643
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-09 18:00:53 +0900

    Title: 최고의 13위치 2, Time: 149ms, Memory: 26MB, Status: Passed - CodeTree

--- 240409/최고의 13위치 2/best-place-of-13-2.py
+++ 240409/최고의 13위치 2/best-place-of-13-2.py
@@ -1,15 +1,20 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
 result = 0
-
 
 for i in range(n):
     for j in range(n - 2):
         one = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
-        for k in range(i + n // 3, n):
-            for l in range(j, n - 2):
-                two = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
-                result = max(result, one + two)
+
+        for k in range(i, n):
+            if k == i:
+                if n >= 6:
+                    for l in range(j + 3, n - 2):
+                        two = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
+                        result = max(result, one + two)
+            else:
+                for l in range(j, n - 2):
+                    two = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
+                    result = max(result, one + two)
 
 print(result)
