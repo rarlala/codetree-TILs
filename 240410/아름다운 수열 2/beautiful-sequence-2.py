96bcf976b120dad5c881b6a4129929f8e9bca41b
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-10 23:46:50 +0900

    Title: 아름다운 수열 2, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/아름다운 수열 2/beautiful-sequence-2.py
+++ 240410/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,13 +2,17 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
+b.sort()
 count = 0
 for i in range(n):
-    arr = set()
-    for c in b:
-        if c in a[i: i + m]:
-            arr.add(a[i: i + m].index(c))
-    if len(a[i: i + m]) == m and len(arr) == len(set(a[i: i + m])) == len(set(b)):
+    temp_a = a[i: i + m]
+    temp_a.sort()
+    
+    check = 0
+    for j in range(m):
+        if len(temp_a) == m and b[j] == temp_a[j]:
+            check += 1
+    if check == m:
         count += 1
 
 print(count)
