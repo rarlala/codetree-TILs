69d1a3023a4953864275f1b98c593e23a698d93d
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-10 23:37:22 +0900

    Title: 아름다운 수열 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/아름다운 수열 2/beautiful-sequence-2.py
+++ 240410/아름다운 수열 2/beautiful-sequence-2.py
@@ -8,7 +8,7 @@ for i in range(n):
     for c in b:
         if c in a[i: i + m]:
             arr.add(a[i: i + m].index(c))
-    if len(a[i: i + m]) == m and len(arr) == m:
+    if len(a[i: i + m]) == m and len(arr) == len(set(a[i: i + m])):
         count += 1
 
 print(count)
