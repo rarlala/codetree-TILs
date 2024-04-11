7fab80a7d2d28a08d358b714cf4c5dcf6843bfdd
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-11 14:38:07 +0900

    Title: 밭의 높이를 고르게하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240411/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,15 @@
+n, h, t = map(int, input().split())
+arr = list(map(int, input().split()))
+
+result = 1e9
+for i in range(n - t):
+    count = 0
+    for j in arr[i: i + t]:
+        if arr[j] == h:
+            continue
+        elif arr[j] > h:
+            count += arr[j] - h
+        elif arr[j] < h:
+            count += h - arr[j]
+    result = min(result, count)
+print(result)
