eb14fa69a8d75cd39aa72d9a7154d6f44713765f
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-14 23:00:33 +0900

    Title: 수 채우기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240414/수 채우기/fill-in-number.py
+++ 240414/수 채우기/fill-in-number.py
@@ -1,10 +1,12 @@
 n = int(input())
 arr = [1e9] * (n + 1)
-arr[2] = 1
-arr[5] = 1
+if n >= 2:
+    arr[2] = 1
+if n >= 5:
+    arr[5] = 1
 
 for i in range(1, n + 1):
     arr[i] = min(arr[i - 2] + 1, arr[i])
     arr[i] = min(arr[i - 5] + 1, arr[i])
 
-print(arr[n])
+print(-1 if arr[n] == 1e9 else arr[n])
