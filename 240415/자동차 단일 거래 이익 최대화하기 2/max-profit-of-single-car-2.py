734ae56de40bcdaa2f3f3a91fdfdfb4b8f4ec3cd
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-15 23:39:51 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 136ms, Memory: 31MB, Status: Passed - CodeTree

--- 240415/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240415/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 result = 0
 
-min_v = 100000
+min_v = 1e9
 for i in range(n):
     min_v = min(min_v, arr[i])
     result = max(result, arr[i] - min_v)
