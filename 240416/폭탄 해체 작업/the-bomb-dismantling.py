5be4b95d41e741a3a250f8c85ec2a0219619926a
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-16 23:28:38 +0900

    Title: 폭탄 해체 작업, Time: 151ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240416/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,13 +1,19 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-arr.sort(key=lambda x: x[1])
+arr.sort(key=lambda x: (-x[0], x[1]))
 
 result = 0
 cur_time = 0
+left_time = 0
 
 for p, t in arr:
     if cur_time < t:
-        cur_time = t
+        cur_time += 1
+        left_time += 1 if t - cur_time - 1 > 0 else 0
+        result += p
+    elif cur_time - left_time < t:
+        left_time -= cur_time - left_time
+        cur_time += 1
         result += p
 
 print(result)
