43496b6afd87a9bb9a18bc5409358a0d76c101b1
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-17 04:44:29 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 473ms, Memory: 33MB, Status: Passed - CodeTree

--- 240417/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240417/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -1,25 +1,37 @@
+from functools import cmp_to_key
+
 n = int(input())
-arr = []
-num_arr = []
+brackets = []
+score = 0
 
 for _ in range(n):
-    data = list(input())
-    l = data.count("(")
-    r = data.count(")")
-    arr.append((l, r, data))
-arr.sort(key=lambda x: (-x[0], x[1]))
+    s = input()
+    o_n, c_n = 0, 0
 
-num = 0
-for i in arr:
-    for j in i[2]:
-        if j == "(":
-            num_arr.append(1)
+    for char in s:
+        if char == "(":
+            o_n += 1
         else:
-            num_arr.append(0)
+            c_n += 1
+            score += o_n
 
-score = 0
-for i in range(len(num_arr) - 1):
-    if num_arr[i] == 1:
-        score += num_arr[i+1:len(num_arr)].count(0)
+    brackets.append((o_n, c_n))
+
+def compare(b1, b2):
+    open1, closed1 = b1
+    open2, closed2 = b2
+
+    if open1 * closed2 > open2 * closed1:
+        return -1
+    if open1 * closed2 < open2 * closed1:
+        return 1
+    return 0
+
+brackets.sort(key=cmp_to_key(compare))
+
+open_sum = 0
+for o_n, c_n in brackets:
+    score += open_sum * c_n
+    open_sum += o_n
 
 print(score)
