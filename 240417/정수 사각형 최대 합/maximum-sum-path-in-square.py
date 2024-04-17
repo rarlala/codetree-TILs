0ac1aeb308d63d387fc3c1db0f7c95014639b284
rarlala <48057629+rarlala@users.noreply.github.com>
2024-04-17 15:23:14 +0900

    Title: 정수 사각형 최대 합, Time: 172ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240417/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+
+dp[0][0] = arr[0][0]
+
+for i in range(1, n):
+    dp[i][0] = dp[i - 1][0] + arr[i][0]
+
+for i in range(1, n):
+    dp[0][i] = dp[0][i - 1] + arr[0][i]
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i - 1][j] + arr[i][j], dp[i][j - 1] + arr[i][j])
+
+print(dp[n - 1][n - 1])
