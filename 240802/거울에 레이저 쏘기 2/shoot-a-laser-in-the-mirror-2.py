d474ec02492106d1ed31a21548cab5ed90e2dba2
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-02 19:55:57 +0900

    Title: 거울에 레이저 쏘기 2, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240802/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -14,7 +14,7 @@ dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
 # 10 20 21 22 6
 #     9  8  7
 
-dir_num = k // n
+dir_num = (k - 1) // n
 # 북 -> x = 0, y = k - 1
 # 동 -> x = (n * 2) - (n + 1), y = n - 1
 # 남 -> x = n - 1, y = (n * 3) - k
@@ -41,26 +41,34 @@ while check:
     if arr[x][y] == '/':
         if dir_num == 0: # 북
             x, y = x + dx[3], y + dy[3]
+            dir_num = 3
         elif dir_num == 1: # 동
             x, y = x + dx[2], y + dy[2]
+            dir_num = 2
         elif dir_num == 2: # 남
             x, y = x + dx[1], y + dy[1]
+            dir_num = 1
         else: # 서
             x, y = x + dx[0], y + dy[0]
+            dir_num = 0
     # \
-    # 북쪽 -> 동쪽
-    # 동쪽 -> 북쪽
-    # 서쪽 -> 남쪽
-    # 남쪽 -> 서쪽
+    # 북쪽 -> 서쪽
+    # 동쪽 -> 남쪽
+    # 서쪽 -> 북쪽
+    # 남쪽 -> 동쪽
     elif arr[x][y] == '\\':
         if dir_num == 0: # 북
-            x, y = x + dx[1], y + dy[1]
-        elif dir_num == 1: # 동
-            x, y = x + dx[0], y + dy[0]
-        elif dir_num == 2: # 남
             x, y = x + dx[3], y + dy[3]
-        else: # 서
+            dir_num = 3
+        elif dir_num == 1: # 동
             x, y = x + dx[2], y + dy[2]
+            dir_num = 2
+        elif dir_num == 2: # 남
+            x, y = x + dx[1], y + dy[1]
+            dir_num = 1
+        else: # 서
+            x, y = x + dx[0], y + dy[0]
+            dir_num = 0
 
     if not 0 <= x < n and 0 <= y < n:
         check = False
