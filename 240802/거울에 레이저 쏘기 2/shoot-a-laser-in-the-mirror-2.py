4736a5deb2b1abda2d3fbab7a4d21f757a234cc5
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-02 19:42:04 +0900

    Title: 거울에 레이저 쏘기 2, Time: 5100ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

---
+++ 240802/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,68 @@
+n = int(input())
+arr = [list(input()) for _ in range(n)]
+k = int(input())
+
+x, y = 0, 0
+
+# k -> k // n
+# 북 동 남 서
+dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+
+#    1  2  3
+# 12 00 01 02 4
+# 11 10 11 12 5
+# 10 20 21 22 6
+#     9  8  7
+
+dir_num = k // n
+# 북 -> x = 0, y = k - 1
+# 동 -> x = (n * 2) - (n + 1), y = n - 1
+# 남 -> x = n - 1, y = (n * 3) - k
+# 서 -> x = (n * 4) - 1 , y = 0
+if dir_num == 0:
+    x, y = 0, k - 1
+elif dir_num == 1:
+    x, y = (n * 2) - (n + 1), n - 1
+elif dir_num == 2:
+    x, y = n - 1,(n * 3) - k
+else:
+    x, y = (n * 4) - 1, 0
+
+count = 0
+check = True
+
+while check:
+    count += 1
+    # /
+    # 북쪽 -> 서쪽
+    # 서쪽 -> 북쪽
+    # 동쪽 -> 남쪽
+    # 남쪽 -> 동쪽
+    if arr[x][y] == '/':
+        if dir_num == 0: # 북
+            x, y = x + dx[3], y + dy[3]
+        elif dir_num == 1: # 동
+            x, y = x + dx[2], y + dy[2]
+        elif dir_num == 2: # 남
+            x, y = x + dx[1], y + dy[1]
+        else: # 서
+            x, y = x + dx[0], y + dy[0]
+    # \
+    # 북쪽 -> 동쪽
+    # 동쪽 -> 북쪽
+    # 서쪽 -> 남쪽
+    # 남쪽 -> 서쪽
+    elif arr[x][y] == '\\':
+        if dir_num == 0: # 북
+            x, y = x + dx[1], y + dy[1]
+        elif dir_num == 1: # 동
+            x, y = x + dx[0], y + dy[0]
+        elif dir_num == 2: # 남
+            x, y = x + dx[3], y + dy[3]
+        else: # 서
+            x, y = x + dx[2], y + dy[2]
+
+    if not 0 <= x < n and 0 <= y < n:
+        check = False
+
+print(count)
