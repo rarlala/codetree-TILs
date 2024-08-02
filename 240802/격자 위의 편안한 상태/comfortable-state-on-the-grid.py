81a17d66423d48c29f44f358246c0d169e10c27e
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-02 18:46:31 +0900

    Title: 격자 위의 편안한 상태, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240802/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,7 +1,7 @@
 n, m = list(map(int, input().split()))
 arr = [[0] * (n + 1) for i in range(n + 1)]
 
-dxs, dys = [0,0,1,-1], [1,1,0,0]
+dxs, dys = [0,0,1,-1], [1,-1,0,0]
 
 for _ in range(m):
     x, y = list(map(int, input().split()))
@@ -12,7 +12,7 @@ for _ in range(m):
         if 0 <= x + dx < n and 0 <= y + dy < n and arr[x + dx][y + dy] == 1:
             count += 1
 
-    if count >= 3:
+    if count == 3:
         print(1)
     else:
         print(0)
